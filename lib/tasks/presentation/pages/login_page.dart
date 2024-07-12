68d64150b5341f0c1f18ae925888fd80510d54d7
paulosavaris <pauloosavaris@gmail.com>
2024-07-12 20:15:48 -0300

    Alterado Imagem/Icon e nome da aplicacao no android

--- lib/tasks/presentation/pages/login_page.dart
+++ lib/tasks/presentation/pages/login_page.dart
@@ -19,6 +19,15 @@ class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
 
+  // Lista de e-mails e senhas pré-definidos
+  final Map<String, String> _userCredentials = {
+    'alice@example.com': 'alice6',
+    'eve@example.com': 'eve123',
+    'ivan@example.com': 'Ivan56',
+    'paulo@example.com': 'paulo6',
+    'stefanie@example.com': 'stefanie',
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,11 +39,29 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            TextField(
-              controller: _emailController,
-              decoration: InputDecoration(
-                labelText: 'E-mail',
-              ),
+            Autocomplete<String>(
+              optionsBuilder: (TextEditingValue textEditingValue) {
+                if (textEditingValue.text.isEmpty) {
+                  return const Iterable<String>.empty();
+                }
+                return _userCredentials.keys.where((String option) {
+                  return option.contains(textEditingValue.text.toLowerCase());
+                });
+              },
+              onSelected: (String selection) {
+                _emailController.text = selection;
+              },
+              fieldViewBuilder:
+                  (context, controller, focusNode, onEditingComplete) {
+                _emailController.value = controller.value;
+                return TextField(
+                  controller: controller,
+                  focusNode: focusNode,
+                  decoration: InputDecoration(
+                    labelText: 'E-mail',
+                  ),
+                );
+              },
             ),
             SizedBox(height: 20),
             TextField(
@@ -47,7 +74,6 @@ class _LoginPageState extends State<LoginPage> {
             SizedBox(height: 20),
             ElevatedButton(
               onPressed: () {
-                // Simulate login button pressed
                 _login(context);
               },
               child: Text('Login'),
@@ -59,47 +85,41 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   void _login(BuildContext context) {
-    // Simulate login logic
     String email = _emailController.text.trim();
     String password = _passwordController.text;
 
-    // Basic validation (you can add more checks here)
+    // Validação básica
     if (email.isEmpty || !email.contains('@')) {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Erro de validação'),
-          content: Text('Por favor, insira um e-mail válido.'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      );
+      _showErrorDialog(context, 'Erro de validação', 'Por favor, insira um e-mail válido.');
       return;
     }
 
     if (password.isEmpty || password.length < 6) {
-      showDialog(
-        context: context,
-        builder: (context) => AlertDialog(
-          title: Text('Erro de validação'),
-          content: Text('A senha deve ter pelo menos 6 caracteres.'),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text('OK'),
-            ),
-          ],
-        ),
-      );
+      _showErrorDialog(context, 'Erro de validação', 'A senha deve ter pelo menos 6 caracteres.');
       return;
     }
 
+    // Autenticação
+    if (_userCredentials[email] == password) {
+      Navigator.pushReplacementNamed(context, Pages.home); // Navegar para a página inicial após login bem-sucedido
+    } else {
+      _showErrorDialog(context, 'Erro de autenticação', 'E-mail ou senha incorretos.');
+    }
+  }
 
-    Navigator.pushReplacementNamed(
-        context, Pages.home); // Navigate to home page after login
+  void _showErrorDialog(BuildContext context, String title, String content) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text(title),
+        content: Text(content),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
   }
 }
